e16a6a33d45b891b0a1d6da9ef2cd7eca6c1a680
CPKR <pavankumarreddychindala@Mac.lan>
2025-09-29 12:58:22 -0500

    authenticate

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
 // for production use
-// export const BASE_URL = "https://just-camera-442001-t3.uc.r.appspot.com/api";
+export const BASE_URL = "https://just-camera-442001-t3.uc.r.appspot.com/api";
 // for backenddevelopment test use
-export const BASE_URL = "https://opulent-sniffle-4jqpv4px5r9x35q94-3000.app.github.dev/api";
+// export const BASE_URL = "https://opulent-sniffle-4jqpv4px5r9x35q94-3000.app.github.dev/api";
